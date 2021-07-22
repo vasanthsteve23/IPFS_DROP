17d5e05fab31b69d3ae0d2f36077911a706ef15a
vasanthsteve23 <80909892+vasanthsteve23@users.noreply.github.com>
2021-07-22 21:20:46 +0530

    IPFS_DROP

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import IPFS from './IPFS';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <IPFS />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
